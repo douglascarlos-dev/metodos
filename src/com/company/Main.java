df1ed8085583ee82b04a204723e6d8d4b9c12d2b
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2022-01-22 15:44:50 -0300

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Calculadora
+        System.out.println("Exercicio calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+    }
+}
